bf37f54e49866e0b8a733841781dd4d40cb9eda5
robertomelo822 <roberto@ezzepay.com.br>
2024-01-29 19:55:59 -0300

    Ajustes em porta

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 21128
+    port: 21128,
+    host: 'soaresinformatica.dev.br',
+    https: false,
   }
 })
